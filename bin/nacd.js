cbf2d363dfd5e230114ca9f19fb12f3e38142446
Gorgi Kosev <gorgi.kosev@gmail.com>
2013-11-04 18:36:11 +0100

    added daemon logging

--- bin/nacd.js
+++ bin/nacd.js
@@ -8,53 +8,78 @@ var net = require('net');
 var dnode = require('dnode');
 var usc = require('unix-socket-credentials');
 var fs = require('fs');
-
+var async = require('async');
 var mkdirp = require('mkdirp');
 var path = require('path');
 
-var LPATH = 
-    process.getuid() 
-        ? path.join(userstore('nac'), 'nacd.sock')
-        : '/tmp/nacd/nacd.sock'
+var isRoot = !process.getuid();
 
+var nacdir = userstore('nac')
+function nacpath(file) {
+    return path.join(nacdir, file);
+}
+var LPATH = isRoot ? '/tmp/nacd/nacd.sock': nacpath('nacd.sock');
 
-if (~process.argv.indexOf('--daemon'))
-    require('daemon')();
+var daemonConfig = require('../lib/daemon/config');
 
+var errlog = fs.createWriteStream(
+        (daemonConfig.stderr || isRoot ? '/var/log/nacd.err.log' 
+            : nacpath('nacd.err.log')), 
+            {flags:'a'}),
+    outlog = fs.createWriteStream(
+        (daemonConfig.stdout || isRoot ? '/var/log/nacd.out.log' 
+            : nacpath('nacd.out.log')), 
+            {flags:'a'});
 
-Daemon.create(function (err, daemon) {
+async.parallel([
+    function(cb) { errlog.once('open', function() { cb(); }) },
+    function(cb) { outlog.once('open', function() { cb(); }) },
+], runDaemon);
 
-    if (err) return console.error('', err.stack) || process.exit(1);
+function runDaemon() {
 
-    if (fs.existsSync(LPATH))
-        fs.unlinkSync(LPATH);
+    if (~process.argv.indexOf('--daemon'))
+        require('daemon')({
+            stdout: outlog,
+            stderr: errlog 
+        });
 
-    mkdirp.sync(path.dirname(LPATH), 0755);
-    fs.chmodSync(path.dirname(LPATH), 0755);
+    Daemon.create(function (err, daemon) {
 
-    net.createServer(serveClient).listen(LPATH, function () {
-        fs.chmodSync(LPATH, 0666);
+        if (err) return console.error('', err.stack) || process.exit(1);
+
+        if (fs.existsSync(LPATH))
+            fs.unlinkSync(LPATH);
+
+        mkdirp.sync(path.dirname(LPATH), 0755);
+        fs.chmodSync(path.dirname(LPATH), 0755);
+
+        net.createServer(serveClient).listen(LPATH, function () {
+            fs.chmodSync(LPATH, 0666);
+        });
+
+        function serveClient(client) {
+            usc.getCredentials(client, function (err, cred) {
+                if (err) return console.log(err);
+                var d = dnode(Daemon.interface(cred.uid, daemon));
+                client.pipe(d).pipe(client);
+            });
+        }
+
+        process.on("uncaughtException", killWorkers);
+        process.on("SIGINT", killWorkers);
+        process.on("SIGTERM", killWorkers);
+        function killWorkers(err) {
+            if (err && err.stack) 
+                console.error(err.stack);
+            else 
+                console.error('Uncaught exception: ' + err);
+            daemon.all().forEach(function(app) {
+                app.kill('SIGTERM', function() {});
+            });
+            process.exit();
+        }
+
     });
-
-    function serveClient(client) {
-        usc.getCredentials(client, function (err, cred) {
-            if (err) return console.log(err);
-            var d = dnode(Daemon.interface(cred.uid, daemon));
-            client.pipe(d).pipe(client);
-        });
-    }
-
-    function killWorkers() {
-        daemon.all().forEach(function(app) {
-            app.kill('SIGTERM', function() {});
-        });
-        process.exit();
-    }
-    process.on("uncaughtException", killWorkers);
-    process.on("SIGINT", killWorkers);
-    process.on("SIGTERM", killWorkers);
-
-});
-
-
+}
 
